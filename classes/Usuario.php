15068f27d0b2be2d18fbf6048fb9c3d242b9a0d5
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2022-10-06 10:38:40 -0300

    Altera a classe de Usuário permitindo agora a adição do Token de confirmãção e bloqueio de conta

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -32,8 +32,13 @@
 		}
 
 		public static function cadastrarUsuario($user, $senha,$nome, $sobrenome, $email, $imagem, $cargo) {
-			$sql = Mysql::conectar()->prepare('INSERT INTO `usuarios` VALUES (null, ?, ?, ?, ?, ?, ?, ?) ');
-			$sql->execute(array($user, $senha,$nome, $sobrenome, $email, $imagem, $cargo));
+			// Trabalhar depois nele
+			$token_confirmacao = "10202003030300303";
+
+			$sql = Mysql::conectar()->prepare('INSERT INTO `usuarios` (id, usuario, senha, nome, sobrenome, email, fotoperfil, acesso, token_confimarcao)
+				VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?) ');
+
+			$sql->execute(array($user, $senha, $nome, $sobrenome, $email, $imagem, $cargo, $token_confirmacao));
 		}
 
 		public static function atualizarUsuarios($nome, $sobrenome, $email, $id) {
