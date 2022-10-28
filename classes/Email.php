ae17064315083fb12d051a06535f017932266771
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2022-10-28 11:12:59 -0300

    Modifica a classe de envio de e-mail

--- classes/Email.php
+++ classes/Email.php
@@ -1,21 +1,19 @@
-<?php  
-	class Email
-	{
+<?php
+	class Email {
 		private $mailer;
-		public function __construct($host,$username,$senha,$name)
-		{
-			
+		public function __construct(){
 			$this->mailer = new PHPMailer;
+			$this->mailer->SMTPDebug = 0;
 
 			$this->mailer->isSMTP();                                      // Set mailer to use SMTP
-			$this->mailer->Host = $host;  				  // Specify main and backup SMTP servers
+			$this->mailer->Host = ENDERECO;  				  // Specify main and backup SMTP servers
 			$this->mailer->SMTPAuth = true;                               // Enable SMTP authentication
-			$this->mailer->Username = $username;                 // SMTP username
-			$this->mailer->Password = $senha;                           // SMTP password
-			$this->mailer->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-			$this->mailer->Port = 465;                                    // TCP port to connect to
+			$this->mailer->Username = USERNAME;                 // SMTP username
+			$this->mailer->Password = SENHA;                           // SMTP password
+			$this->mailer->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$this->mailer->Port = 587;                                    // TCP port to connect to
 
-			$this->mailer->setFrom($username,$name);
+			$this->mailer->setFrom(USERNAME, NAME);
 			$this->mailer->isHTML(true);                                  // Set email format to HTML
 			$this->mailer->CharSet = 'UTF-8';
 
@@ -25,10 +23,18 @@
 			$this->mailer->addAddress($email,$nome);		
 		}
 
-		public function formatarEmail($info) {
-			$this->mailer->Subject = $info['assunto'];
-			$this->mailer->Body = $info['corpo'];
-			$this->AltBody = strip_tags($info['corpo']);
+		public function EmailConfirmacao($nome, $login, $token_confirmacao) {
+			$url = INCLUDE_PATH_PAINEL . "confirmar-email?token_confirmacao=" . $token_confirmacao;
+			$this->mailer->Subject = 'Confirmação de e-mail.';
+			$message = file_get_contents(__DIR__ . '/phpmailer/confirmacao.html');
+			$message = str_replace('%ano_atual%', date('Y'), $message);
+			$message = str_replace('%nome_empresa%', NOME_EMPRESA, $message);
+			$message = str_replace('%login_url%', INCLUDE_PATH_PAINEL, $message);
+			$message = str_replace('%username%', $login, $message);
+			$message = str_replace('%nome%', $nome, $message);
+			$message = str_replace('action_url', $url, $message);
+
+			$this->mailer->msgHTML($message, __DIR__);
 		}
 
 		public function enviarEmail() {
