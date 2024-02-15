bab2b68a81576b985fbc79e915130a59a72dd0e4
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2024-02-15 17:46:11 -0300

    feat: Menu foi convertido de JQuery para JS puro

--- painel/js/menu.js
+++ painel/js/menu.js
@@ -1,58 +1,107 @@
-$(function(){
+// Variável de controle para verificar se o menu está aberto.
+let open = true;
 
-	var open  = true;
-	var windowSize = $(window)[0].innerWidth;
+document.addEventListener('DOMContentLoaded', function () {
+  let menu = document.querySelector('.menu');
+  let menuBtn = document.querySelector('.menu-btn');
+  let header = document.querySelector('header');
+  let content = document.querySelector('.content');
 
-	var targetSizeMenu = (windowSize <= 400) ? 200 : 250;
+  // Pega o tamanho da tela atual. 
+  let windowSize = window.innerWidth;
 
-	if(windowSize <= 768){
-		$('.menu').css('width','0').css('padding','0');
-		open = false;
-	}
+  // Armazena o novo tamanho do menu.
+  let targetSizeMenu = windowSize <= 400 ? 200 : 250;
 
-	$('.menu-btn').click(function(){
-		if(open){
-			//O menu está aberto, precisamos fechar e adaptar nosso conteudo geral do painel
-			$('.menu').animate({'width':0,'padding':0},function(){
-				open = false;
-			});
-			$('.content,header').css('width','100%');
-			$('.content,header').animate({'left':0},function(){
-				open = false;
-			});
-		}else{
-			//O menu está fechado
-			$('.menu').css('display','block');
-			$('.menu').animate({'width':targetSizeMenu+'px','padding':'10px 0'},function(){
-				open = true;
-			});
-			if(windowSize > 768)
-				$('.content,header').css('width','calc(100% - 250px)');
-				$('.content,header').animate({'left':targetSizeMenu+'px'},function(){
-				open = true;
-			});
-		}
-	})
+  //Verifica se o tamanho da tela é menor que 768
+  if (windowSize <= 768) {
+    // Caso verdade altera o tamanho e o padding para 0 e
+    // define o valor de open para false.
+    console.log("menu sumiu");
+    menu.style.width = 0;
+    menu.style.padding = 0;
+    open = false;
+  }
 
-	$(window).resize(function(){
-		windowSize = $(window)[0].innerWidth;
-		targetSizeMenu = (windowSize <= 400) ? 200 : 250;
-		if(windowSize <= 768){
-			$('.menu').css('width','0').css('padding','0');
-			$('.content,header').css('width','100%').css('left','0');
-			open = false;
-		}else{
-			$('.menu').animate({'width':targetSizeMenu+'px','padding':'10px 0'},function(){
-				open = true;
-			});
+  content.style.transitionTimingFunction = "linear";
+  content.style.transition = 'left 0.5s'; // Define a duração da animação
 
-			$('.content,header').css('width','calc(100% - 250px)');
-			$('.content,header').animate({'left':targetSizeMenu+'px'},function(){
-			open = true;
-			});
-		}
+  header.style.transitionTimingFunction = "linear";
+  header.style.transition = 'all 0.5s'; // Define a duração da animação
 
-	})
+  menu.style.transitionTimingFunction = "linear";
+  menu.style.transition = 'all 0.5s'; // Define a duração da animação  
+  
+  menuBtn.addEventListener('click', function () {
+    // O menu está aberto, precisamos fechar e adaptar nosso conteúdo geral do painel
+    console.log("Menu fechou");
+    if (open) {      
+      content.style.width = '100%'
+      content.style.left = '0'
 
-	$('[formato=data]').mask('99/99/9999');
-})
+      header.style.width = '100%';
+      header.style.left = '0';
+
+      menu.style.width = '0';
+      menu.style.padding = '0';
+
+      open = false;
+    }
+
+    else {
+      //O menu está fechado
+      console.log("Menu abriu");
+    
+      menu.style.display = 'block';
+      menu.style.width = `${targetSizeMenu}px`;
+      menu.style.padding = '10px 0';
+
+      header.style.left = `${targetSizeMenu}px`;
+
+      content.style.left = `${targetSizeMenu}px`;
+      
+      if(windowSize > 768) {
+        header.style.width = 'calc(100% - 250px)';
+        header.style.left = `${targetSizeMenu}px`;
+
+        content.style.width = 'calc(100% - 250px)';
+        content.style.left = `${targetSizeMenu}px`;
+      }      
+
+      open = true;    
+    }
+  });
+
+  window.addEventListener('resize', function() {
+    let windowSize = window.innerWidth;
+    let targetSizeMenu = (windowSize <= 400) ? 200 : 250;
+
+    console.log(windowSize, window.innerWidth, targetSizeMenu);
+
+    if(windowSize <= 768) {
+      menu.style.width = '0';
+      menu.style.padding = '0px';
+
+      content.style.width = '100%';
+      content.style.left = '0';
+
+      header.style.width = '100%';
+      header.style.left = '0';
+
+      open = false;
+    } 
+
+    else {
+      menu.style.width = `${targetSizeMenu}px`;
+      menu.style.padding = '10px 0';
+      header.style.width = 'calc(100% - 250px)';
+      header.style.left = `${targetSizeMenu}px`;
+
+      content.style.width = 'calc(100% - 250px)';
+      content.style.left = `${targetSizeMenu}px`;
+      
+      open = true;
+    }
+  });
+});
+
