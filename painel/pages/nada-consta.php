6df648d94592da23b29d9533e821111b2d616ee2
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2024-07-17 18:55:11 -0300

    fix: Corrige erro do TCPDF

--- painel/pages/nada-consta.php
+++ painel/pages/nada-consta.php
@@ -3,17 +3,16 @@
 ?>
 
 <div class="box-content">
+	<?php 
+		if(isset($_POST['acao'])) {
+			$acaoNadaConsta = (int) filter_var($_POST["tipo"], FILTER_SANITIZE_NUMBER_INT);
+			NadaConsta::geraNadaConsta($acaoNadaConsta);
+	    }
+	?>
 	<h2> <i class="fas fa-file-alt"></i> Nada Consta</h2>
 	<p>Esta função consiste em validar se não existem pendências ou registros negativos associados ao seu perfil.</p>
 
-	<form method="post">
-		<?php 
-			if(isset($_POST['acao'])) {
-				$acaoNadaConsta = (int) filter_var($_POST["tipo"], FILTER_SANITIZE_NUMBER_INT);
-				NadaConsta::geraNadaConsta($acaoNadaConsta);
-	        }
-		?>
-		
+	<form method="post">		
 		<div class="form-group">
 			<label for="tipo">Escolha o que fazer com a declaração:</label>
 			<select name="tipo" id="tipo">
